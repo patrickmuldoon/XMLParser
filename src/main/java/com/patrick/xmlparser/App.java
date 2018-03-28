0081777d390a62205494ed68080a431b4219730d
Patrick <patrickmuldoon20@yahoo.com>
2018-03-27 20:33:39 -0700

    Initializing repository

---
+++ src/main/java/com/patrick/xmlparser/App.java
@@ -0,0 +1,13 @@
+package com.patrick.xmlparser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
